9e5039ab86e22e3766c9c4d901b522fec5c503a6
IkrtI <minipro787@gmail.com>
2026-02-20 17:45:18 +0700

    p03

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,1 +1,20 @@
-export class Character {}
+export class Character {
+  name: string = "";
+  health: number = 100;
+
+  constructor(name: string, health: number = 100) {
+    this.name = name;
+    this.health = health;
+  }
+
+  getName() : string {
+    return this.name;
+  }
+  getHealth() : number {
+    return this.health;
+  }
+  receiveDamage(damage: number)  {
+    damage = this.name === "King Arthur" ? damage * 0.9 : damage;
+    this.health -= damage;
+  }
+}
