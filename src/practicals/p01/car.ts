b7f7401923d36dae702e0853de659dbdd30c2386
IkrtI <minipro787@gmail.com>
2026-02-20 17:26:41 +0700

    CAT!

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,17 @@
-export class Car {}
+export class Car {
+    public brand: string = "";
+    public model: string = "";
+
+    start(): void{
+        console.log(`${this.brand} ${this.model} is starting...`);
+    }
+    accelerate(): void{
+        console.log(`${this.brand} ${this.model} is accelerating...`);
+    }
+    brake(): void{
+        console.log(`${this.brand} ${this.model} is braking...`);
+    }
+    stop(): void{
+        console.log(`${this.brand} ${this.model} is stopping...`);
+    }
+}
